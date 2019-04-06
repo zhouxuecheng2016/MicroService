d7975942653f8e89ecf394df23da9ac7a024ebdb
周学成 <zhouxuecheng_2012@163.com>
2019-04-06 21:37:28 +0800

    micro commit

---
+++ MicroChain/src/main/java/com/demand/micro/App.java
@@ -0,0 +1,13 @@
+package com.demand.micro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
